b04e3763f51235fe3c53c12265325cce9aa76119
Adrian McShane <mcshaneadrian@gmail.com>
2023-06-12 10:34:28 +0100

    prize should be integer

--- db/migrate/20230606140430_add_prize_position_to_prizes.rb
+++ db/migrate/20230606140430_add_prize_position_to_prizes.rb
@@ -1,5 +1,5 @@
 class AddPrizePositionToPrizes < ActiveRecord::Migration[7.0]
   def change
-    add_column :prizes, :prize_position, :string
+    add_column :prizes, :prize_position, :integer
   end
 end
