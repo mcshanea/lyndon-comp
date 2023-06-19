911ec20d2971ad05f357c561e99d64abbd9cdbb4
Adrian McShane <mcshaneadrian@gmail.com>
2023-06-19 12:32:17 +0100

    corrected logic for incorrect inclusions

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -10,7 +10,7 @@ class Applicant < ApplicationRecord
     competition = Competition.first
 
     CSV.foreach(file.path, headers: true, :header_converters => lambda { |h| h.downcase.gsub(' ', '_') }) do |row|
-      if Applicant.find_by_email( row["email"] ).nil? == false && row["answer"] != "74" && row["answer"] != "70"
+      if row["answer"] != "74" && row["answer"] != "70"
         if row["answer"].blank?
           check_for_multiple << row
         end
@@ -39,7 +39,8 @@ class Applicant < ApplicationRecord
     check_for_multiple.each do |row|
       applicant = Applicant.find_by_email( row["email"] )
 
-      if applicant.nil? == false && applicant.answer == 74
+      # to exist in the DB they must have entered a right answer
+      if applicant.nil? == false
         row["competition_id"] = competition.id
         Applicant.create!(row.to_h)
       end
