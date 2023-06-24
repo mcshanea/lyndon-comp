4484cb6f17c01b7bfd4b2e058783112ea3135ee0
Adrian McShane <mcshaneadrian@gmail.com>
2023-06-24 15:15:20 +0100

    added email template

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -6,6 +6,7 @@ class Prize < ApplicationRecord
     winner = applicants.where(won: nil).sample
     winner.update_attribute(:won, true)
     update(winner_id: winner.id) # Assuming the winner_id column exists in the Prize model
+    winner.save
     winner
   end
 end
